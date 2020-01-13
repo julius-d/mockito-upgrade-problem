662b512fe0f64dffe7a876c916f8abb986acf05a
Julius <30121440+julius-d@users.noreply.github.com>
2020-01-13 09:33:16 +0100

    Make example project smaller but still failing

--- src/main/java/com/example/mockitoupgradeproblem/MyDepObj.java
+++
@@ -1,5 +0,0 @@
-package com.example.mockitoupgradeproblem;
-
-public class MyDepObj {
-
-}
--- src/main/java/com/example/mockitoupgradeproblem/MyObject.java
+++ src/main/java/com/example/mockitoupgradeproblem/MyObject.java
@@ -2,10 +2,4 @@ package com.example.mockitoupgradeproblem;
 
 public class MyObject {
 
-  private final MyDepObj myDepObj;
-
-  public MyObject(MyDepObj myDepObj) {
-    this.myDepObj = myDepObj;
-  }
-
 }

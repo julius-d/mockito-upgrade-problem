cef026be9a997e0632bd074907eb1e459281db06
Szczepan Faber <sfaber@linkedin.com>
2020-01-13 20:08:24 -0800

    Updated the code so that it's easier to reproduce and debug https://github.com/mockito/mockito/issues/1857

--- src/test/java/com/example/mockitoupgradeproblem/a/A2Test.java
+++ src/test/java/com/example/mockitoupgradeproblem/a/A2Test.java
@@ -4,26 +4,30 @@ import static org.junit.Assert.assertEquals;
 
 import com.example.mockitoupgradeproblem.MyObject;
 import java.util.stream.IntStream;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
 
 @RunWith(MockitoJUnitRunner.class)
 public class A2Test {
+
+//  public @Rule MockitoRule mockito = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
 
   @Mock
   private MyObject myObject;
 
   @Test
   public void name() {
-    IntStream.range(0,  Integer.MAX_VALUE).boxed()
-        .forEach(i -> assertEquals(i, i));
-  }
-
-  @Test
-  public void name2() {
-    IntStream.range(0,  Integer.MAX_VALUE).boxed()
-        .forEach(i -> assertEquals(i, i));
+    try {
+      Thread.sleep(100);
+    } catch (InterruptedException e) {
+      //
+    }
   }
 }
